69193460047d7af6ca74b93dea26f7eda0cf82e8
easygautam <gauta90055@gmail.com>
2022-10-11 11:51:26 +0530

    Varible lecture added

---
+++ src/variable/AddTwoNumber.java
@@ -0,0 +1,19 @@
+package variable;
+
+public class AddTwoNumber {
+
+    public static void main(String[] args) {
+        // Add two numbers
+
+        int n1 = 10;
+        int n2 = 15;
+
+        int sum = n1 + n2;
+
+        // String concatination
+        // Hello + 1 = Hello1
+        // 1 + 1 = 2
+        // 1 + Hello = 1Hello
+        System.out.println("Sum is " + sum);
+    }
+}
---
+++ src/variable/VariableExample.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class VariableExample {
+
+    public static void main(String[] args) {
+        // Variables and data types
+
+        // Define a variable
+        int age = 1;
+        age  = 2;
+
+        // Printing the value of age
+        System.out.println(age);
+
+    }
+}
