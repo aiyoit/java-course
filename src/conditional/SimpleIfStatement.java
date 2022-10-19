0fa88a40ebc323a936bf64dcfd788273c7ccc7a8
easygautam <gauta90055@gmail.com>
2022-10-19 10:44:05 +0530

    Simple if statement added

---
+++ src/conditional/SimpleIfStatement.java
@@ -0,0 +1,27 @@
+package conditional;
+
+public class SimpleIfStatement {
+
+    public static void main(String[] args) {
+        // If true code will be executed
+        if (true) {
+            System.out.println("Hello True");
+        }
+
+        // If false code willn't be executed
+        if (false) {
+            System.out.println("Hello False");
+        }
+
+        // Check age should be greater than 18
+
+
+        int age = 19;
+
+        if (age > 18) {
+            System.out.println("You are allowed");
+        }
+
+    }
+
+}
