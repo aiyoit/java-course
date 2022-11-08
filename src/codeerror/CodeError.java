f3ac4be4b8deb8215ed5fd04d65a8c8c707f666c
easygautam <gauta90055@gmail.com>
2022-11-08 09:58:45 +0530

    Code error understanding

---
+++ src/codeerror/CodeError.java
@@ -0,0 +1,29 @@
+package codeerror;
+
+public class CodeError {
+
+    public static void main(String[] args) {
+//        int a = 1;
+//        switch(a) {
+//
+//            case 1 :
+//
+//                System.out.println("1");
+//
+//            case 2:
+//                System.out.println("2");
+//                System.out.println("22");
+//                break;
+//
+//        }
+
+
+        int a = 0;
+        int i;
+        for ( i = 2; i < 1000; i = i + 2) {
+            a = a + i;
+        }
+
+    }
+
+}
